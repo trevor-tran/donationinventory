87c61af4a6a7b96d88858be8e6eb8e29c46ec314
Trevor Tran <fuongdtran@gmail.com>
2024-03-25 20:40:06 -0700

    init commit

---
+++ src/main/java/com/trevortran/donationinventory/DonationinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.trevortran.donationinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationinventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trevortran/donationinventory/DonationinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevortran.donationinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
